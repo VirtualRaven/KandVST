60216cf0ddf4af15c5054131321c3b89d5f757ee
Sarosh Nasir <sarosh.nasir@gmail.com>
2018-02-25 15:36:09 +0100

    Remove some images, try to include OurLookAndFeel
    For some reason all previous code i had written for OurLookAndFeel was
    not in the .cpp and .h files (not A LOT but stilll). So i'm trying to
    see if it'll save the changes now.

---
+++ Code/Plugin/Source/OurLookAndFeel.h
@@ -0,0 +1,1 @@
+// DOOO SOMETHINGs
--- Code/Plugin/Source/Resources_files.h
+++ Code/Plugin/Source/Resources_files.h
@@ -6,7 +6,7 @@ extern char refresh_png[8494];
 }
 namespace Images{
 extern char Title_png[18963];
-extern char Title2_png[41600];
+extern char knobs_png[10266];
 extern char Title3_png[9116];
 }
 }
