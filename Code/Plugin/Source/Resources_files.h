86cbedf71e64839cad9ecf3b9202c70828129662
mightynerd <staszw@gmail.com>
2018-03-05 15:51:07 +0100

    Change ExampleEffect to DelayEffect in CMakeLists

--- Code/Plugin/Source/Resources_files.h
+++ Code/Plugin/Source/Resources_files.h
@@ -1,13 +1,13 @@
 #ifndef RESOURCES_FILES_H
 #define RESOURCES_FILES_H
-namespace Resources {
-	namespace Icons {
-		extern unsigned char knobs_png[10266];
-		extern unsigned char refresh_png[8494];
-	}
-	namespace Images {
-		extern unsigned char loading_png[30548];
-		extern unsigned char Title3_png[9116];
-	}
+namespace Resources{
+namespace Icons{
+extern unsigned char knobs_png[10266];
+extern unsigned char refresh_png[8494];
+}
+namespace Images{
+extern unsigned char loading_png[30548];
+extern unsigned char Title3_png[9116];
+}
 }
 #endif
