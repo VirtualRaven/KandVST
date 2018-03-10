6e0e537e76435c008fdc14d024632ee731671359
mightynerd <staszw@gmail.com>
2018-03-10 14:43:16 +0100

    Add labels (not positioned) to SelectKnob

--- Code/Plugin/Source/Components/SelectKnob.cpp
+++ Code/Plugin/Source/Components/SelectKnob.cpp
@@ -1,45 +1,31 @@
-/*
-==============================================================================
-
-This file is part of the JUCE library.
-Copyright (c) 2017 - ROLI Ltd.
-
-JUCE is an open source library subject to commercial or open-source
-licensing.
-
-By using JUCE, you agree to the terms of both the JUCE 5 End-User License
-Agreement and JUCE 5 Privacy Policy (both updated and effective as of the
-27th April 2017).
-
-End User License Agreement: www.juce.com/juce-5-licence
-Privacy Policy: www.juce.com/juce-5-privacy-policy
-
-Or: You may also use this code under the terms of the GPL v3 (see
-www.gnu.org/licenses).
-
-JUCE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY, AND ALL WARRANTIES, WHETHER
-EXPRESSED OR IMPLIED, INCLUDING MERCHANTABILITY AND FITNESS FOR PURPOSE, ARE
-DISCLAIMED.
-
-==============================================================================
-*/
-
 
 #include "SelectKnob.h"
 
-
-
 SelectKnob::SelectKnob(AudioParameterChoice& p) :
-	ParameterSlider(p), param(p)
+	ParameterSlider(p),
+	__labels(),
+	__param(p)
 {
 	updateSliderPos();
 	setSliderStyle(Slider::RotaryHorizontalVerticalDrag);
 	setTextBoxStyle(Slider::NoTextBox, false, 0, 0);
+
+	for (int i = 0; i < __param.choices.size(); i++)
+	{
+		Label* l = new Label(__param.choices[i], __param.choices[i]);
+		l->setText(__param.choices[i], NotificationType::sendNotification);
+		__labels.push_back(l);
+		addAndMakeVisible(*l);
+	}
 }
 
 
 SelectKnob::~SelectKnob()
 {
+	for (int i = 0; i < __labels.size(); i++)
+	{
+		delete __labels.at(i);
+	}
 }
 
 void SelectKnob::valueChanged()
@@ -54,6 +40,24 @@ void SelectKnob::startedDragging()
 void SelectKnob::stoppedDragging()
 { 
 	param.endChangeGesture(); 
+}
+
+void SelectKnob::paint(Graphics& g) {
+	ParameterSlider::paint(g);
+
+	g.setColour(Colour::fromRGB(255, 40, 40));
+	g.drawRect(getBounds().reduced(10));
+}
+
+void SelectKnob::resized()
+{
+	Rectangle<int> box(getLocalBounds());
+	for (int i = 0; i < __labels.size(); i++)
+	{
+		__labels.at(i)->setBounds(box);
+	}
+
+	ParameterSlider::resized();
 }
 
 
--- Code/Plugin/Source/Components/SelectKnob.h
+++ Code/Plugin/Source/Components/SelectKnob.h
@@ -1,30 +1,3 @@
-/*
-==============================================================================
-
-This file is part of the JUCE library.
-Copyright (c) 2017 - ROLI Ltd.
-
-JUCE is an open source library subject to commercial or open-source
-licensing.
-
-By using JUCE, you agree to the terms of both the JUCE 5 End-User License
-Agreement and JUCE 5 Privacy Policy (both updated and effective as of the
-27th April 2017).
-
-End User License Agreement: www.juce.com/juce-5-licence
-Privacy Policy: www.juce.com/juce-5-privacy-policy
-
-Or: You may also use this code under the terms of the GPL v3 (see
-www.gnu.org/licenses).
-
-JUCE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY, AND ALL WARRANTIES, WHETHER
-EXPRESSED OR IMPLIED, INCLUDING MERCHANTABILITY AND FITNESS FOR PURPOSE, ARE
-DISCLAIMED.
-
-==============================================================================
-*/
-
-
 
 #ifndef SELCT_KNOB_H
 #define SELCT_KNOB_H
@@ -37,11 +10,15 @@ public:
 	SelectKnob(AudioParameterChoice& p);
 	~SelectKnob();
 	void valueChanged() override;
+	void resized() override;
+	void paint(Graphics& g) override; // Temp?
 
 	void startedDragging() override;
 	void stoppedDragging() override;
 
-	AudioParameterChoice& param;
+private:
+	std::vector<Label*> __labels;
+	AudioParameterChoice& __param;
 };
 
 
