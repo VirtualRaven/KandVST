912c3bd0d11212bad9a4e9caa993ddcfdf186e16
klas <klas@ludvigsson.se>
2018-03-22 15:40:30 +0100

    Change range on dist

--- Code/Plugin/Source/DistEffect.cpp
+++ Code/Plugin/Source/DistEffect.cpp
@@ -16,22 +16,22 @@ DistEffect<T>::~DistEffect()
 template<typename T>
 void DistEffect<T>::RegisterParameters(int ID)
 {
-	Global->paramHandler->RegisterFloat(ID, "DIST_TRSH", "Distortion threshold",0.0f,2.0f,2.0f);
+	Global->paramHandler->RegisterFloat(ID, "DIST_TRSH", "Distortion threshold",0.0f,1.0f,1.0f);
 	Global->paramHandler->RegisterBool(ID, "DIST_EN", "Distortion enable", false);
 }
 
 template<typename T>
 bool DistEffect<T>::RenderBlock(AudioBuffer<T>& buffer, int len, bool empty) {
 	if (empty || !(*__isActive)) return false;
+	double t = (*__threshold);
 	auto buff = buffer.getArrayOfWritePointers();
 	for (int i = 0; i < len; i++) {
 		double samp0 = buff[0][i];
 		double samp1 = buff[1][i];
 		int mul1 = samp0 < 0 ? -1 : 1;
 		int mul2 = samp1 < 0 ? -1 : 1;
-		double t = (*__threshold);
-		buff[0][i] = std::min(abs(samp0),t) * mul1;
-		buff[1][i] = std::min(abs(samp1),t) * mul2;
+		buff[0][i] = std::min(abs(samp0),1.1-t) * mul1;
+		buff[1][i] = std::min(abs(samp1),1.1-t) * mul2;
 	}
 	return true;
 }
