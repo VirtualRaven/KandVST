c0b3183981cf9ee3be4c4c52bbfacc5cba983c7b
Sarosh Nasir <sarosh.nasir@gmail.com>
2018-03-25 03:28:19 +0200

    new wave icons

--- Code/Plugin/Source/Util/Resources_files.h
+++ Code/Plugin/Source/Util/Resources_files.h
@@ -4,15 +4,20 @@ namespace Resources{
 namespace Icons{
 extern unsigned char delete_png[542];
 extern unsigned char knobs_png[10266];
+extern unsigned char noise_png[16692];
 extern unsigned char refresh_png[8494];
 extern unsigned char save_png[1216];
+extern unsigned char saw_png[12719];
+extern unsigned char sine_png[13820];
 extern unsigned char sliderthumb_png[10575];
+extern unsigned char square_png[9502];
 extern unsigned char tb_active_png[4316];
 extern unsigned char tb_active_master_png[4240];
 extern unsigned char tb_inactive_png[3410];
+extern unsigned char triangel_png[13914];
 }
 namespace Images{
-extern unsigned char loading_png[30548];
+extern unsigned char loading_png[35309];
 extern unsigned char Title3_png[12272];
 }
 }
