0deac8f8a2b1c059a8af054543d768f60c7d3e4f
klas <klas@ludvigsson.se>
2018-04-04 16:21:44 +0200

    Add test for Dist

--- Code/Plugin/Source/Effects/DistEffect.cpp
+++ Code/Plugin/Source/Effects/DistEffect.cpp
@@ -20,7 +20,7 @@ DistEffect<T>::~DistEffect()
 template<typename T>
 void DistEffect<T>::RegisterParameters(int ID, GLOBAL*Global)
 {
-	Global->paramHandler->RegisterFloat(ID, "DIST_TRSH", "Distortion threshold",0.0f,0.99f,0.0f);
+	Global->paramHandler->RegisterFloat(ID, "DIST_TRSH", "Distortion threshold",0.0f,0.95f,0.0f);
 	Global->paramHandler->RegisterBool(ID, "DIST_EN", "Distortion enable", false);
 	Global->paramHandler->RegisterInt(ID, "DIST_LFO", "Dist lfo", 0, 2, 0);
 }
@@ -41,10 +41,10 @@ bool DistEffect<T>::RenderBlock(AudioBuffer<T>& buffer, int len, bool empty) {
 	for (int i = 0; i < len; i++) {
 		double samp0 = buff[0][i];
 		double samp1 = buff[1][i];
-		int mul1 = samp0 < 0 ? -1 : 1;
-		int mul2 = samp1 < 0 ? -1 : 1;
-		buff[0][i] = std::min(abs(samp0),1.1-t) * mul1;
-		buff[1][i] = std::min(abs(samp1),1.1-t) * mul2;
+		int mul0 = samp0 < 0 ? -1 : 1;
+		int mul1 = samp1 < 0 ? -1 : 1;
+		buff[0][i] = std::min(abs(samp0),1.0-t) * mul0;
+		buff[1][i] = std::min(abs(samp1),1.0-t) * mul1;
 		
 		/*double k = 2.0 * t / (1.0 - t);
 		buff[0][i] = (1.0 + k)*samp0 / (1.0 + k*abs(samp0));
---
+++ Code/Plugin/Tester/Tests/Dist_Verification/Dist_Verification.h
@@ -0,0 +1,46 @@
+#ifndef DIST_VERIFICATION_H
+#define DIST_VERIFICATION_H
+#include "TestHost.h"
+class Dist_Verification : public TestHost::Test {
+	
+	virtual const char * name() const {
+		return "Dist_Verification";
+	};
+	
+	virtual bool run(wrapperVST* vst, 
+		std::stringstream& msg, 
+		const TestHost::ParamsData& d) {
+				HParameterChanges c;
+				HEventList events;
+				//Create a single note on
+				Steinberg::Vst::Event e;
+				e.busIndex = 0;
+				e.sampleOffset = 0;
+				e.type = Steinberg::Vst::Event::kNoteOnEvent;
+				e.noteOn = {
+					0,
+					40,
+					0.0,
+					1.0,
+					0,
+					-1
+
+				};
+				events.addEvent(e);
+				return this->block.process(vst, &c,&events);
+			}
+			
+	virtual bool exportTestData() {
+		return true;
+	}
+			
+	virtual bool hasPythonStep() {
+		return true;
+	}
+			
+	virtual bool hasParameterFile() {
+		return true;
+	}
+};
+
+#endif
